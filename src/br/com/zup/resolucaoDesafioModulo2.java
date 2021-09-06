351e9ac7e1d9a803d3c32f618e20aeb2eb6031f0
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-06 03:38:52 -0300

    Criar entrada de dados básica do cadastro (entrega mínima)

--- src/br/com/zup/resolucaoDesafioModulo2.java
+++ src/br/com/zup/resolucaoDesafioModulo2.java
@@ -11,5 +11,17 @@ public class resolucaoDesafioModulo2 {
         Map<String, String> funcionarios = new HashMap<String, String>();
         boolean operarMenu = true;
 
+        //Entrega mínima:
+
+        System.out.println("Olá, seja bem vinde ao nosso sistema de gerenciamento de funcionários!");
+        System.out.println("Por favor, digite o nome do funcionário que gostaria de cadastrar: ");
+        String nome = input.nextLine();
+        System.out.println("Por favor, insira o telefone do funcionário: ");
+        String telefone = input.nextLine();
+        System.out.println("Por favor, insira o email do funcionário: ");
+        String email = input.nextLine();
+        System.out.println("Por favor, insira o CPF do funcionário: ");
+        String cpf = input.nextLine();
+
     }
 }
