d9002736894801796c98e558968e99b953d4c5be
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-06 03:28:38 -0300

    Criar Scanner + HashMap + variável booleana

---
+++ src/br/com/zup/resolucaoDesafioModulo2.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class resolucaoDesafioModulo2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Map<String, String> funcionarios = new HashMap<String, String>();
+        boolean operarMenu = true;
+
+    }
+}
